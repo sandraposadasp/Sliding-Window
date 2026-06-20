f7087ae736a531124a2f789763a3696d6d41ab01
Sandra <hi-sandra@hotmail.com>
2026-06-20 13:52:47 -0600

    recorrer arreglo

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function findLongestWord(text) {
     let words = text.split(" ");
     let longestWord = ''; 
 
+     for (let word of words) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+
